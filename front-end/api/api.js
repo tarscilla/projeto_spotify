088aaf4e8a3d5ab69f5d82218b7ed3bdaacf92ce
Tarscilla Almeida <113654299+tarscilla@users.noreply.github.com>
2025-08-14 13:19:40 -0300

    Update api.js

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:3001";
+const URL = "https://projeto-spotify-back-end.onrender.com";
 
 const responseArtists = await axios.get(`${URL}/artists`); 
 const responseSongs = await axios.get(`${URL}/songs`); 
